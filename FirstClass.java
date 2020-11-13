6f065ccca419832302172590fddeed2586be85d6
bharat <bharatpipare987@gmail.com>
2020-11-13 22:03:06 +0530

    Working with Java Fundamentals - Variables,Datatypes,Implicit conversion & Typecasting

--- FirstClass.java
+++ FirstClass.java
@@ -1,5 +1,35 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.print("Hello World");
+        /**
+         * Range for integer is -2,147,483,648 to 2,147,483,647
+         */
+        // int num = 500000000;
+        // System.out.println(num);
+
+        // int number = 6;
+        // number = 50_00_00_000;
+        // System.out.println(number);
+
+        // float f = 5.5f;
+        // System.out.println(f);
+
+        // double percent = 5.5;
+        // System.out.println(percent);
+
+        // long big_value = 5l;
+        // System.out.println(big_value);
+        // long l = 500000000l;
+
+        // char c = 'A';
+        // c = 66; // ASCII
+        // c++;
+        // System.out.println(c);
+
+        // double d1 = 5; // Implicit conversion
+        // System.out.println(d1);
+
+        int k = (int) 5.6; // Typecasting
+        System.out.println(k);
+
     }
 }
