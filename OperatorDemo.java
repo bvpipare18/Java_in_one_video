ccb910e0df5f9010c5c9181f4c1bc98ce119a04d
bharat <bharatpipare987@gmail.com>
2020-11-14 11:51:12 +0530

    Operators - Arithmetic Operators in Java

---
+++ OperatorDemo.java
@@ -0,0 +1,44 @@
+public class OperatorDemo {
+    /**
+     * Arithmetic --> +,-,*,/,%
+     * 
+     * Bitwise Relational Logical
+     */
+    public static void main(String[] args) {
+        // int m = 6, n = 2;
+
+        // int addition = m + n;
+        // int subtract = m - n;
+        // int multiplication = m * n;
+        // int division = m / n;
+        // int modulas = m % n;
+
+        // n = 4;
+        // double division1 = (double) m / n; // Typecasting
+        // int division2 = m / n;
+
+        // System.out.println(addition);
+        // System.out.println(subtract);
+        // System.out.println(multiplication);
+        // System.out.println(division);
+        // System.out.println(modulas);
+        // System.out.println(division1);
+        // System.out.println(division2);
+
+        // System.out.println(n += m);
+
+        // System.out.println(++n);
+        // System.out.println(n++);
+
+        // ++n; // pre increment
+        // ++n; // post increment
+        // int a = ++m;
+        // System.out.println(a);
+        // System.out.println(m);
+
+        // int b = n++;
+        // System.out.println(b);
+        // System.out.println(n);
+
+    }
+}
