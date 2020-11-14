58ad535bc57ffc20d3be2a28f7a8dacde668dbc9
bharat <bharatpipare987@gmail.com>
2020-11-14 12:20:56 +0530

    if-else statements

---
+++ SelectionDemo.java
@@ -0,0 +1,14 @@
+public class SelectionDemo {
+    public static void main(String[] args) {
+        int n = 7;
+        if (n == 0) {
+            System.out.println("Nothing");
+        } else {
+            if (n % 2 == 0) {
+                System.out.println("Even");
+            } else {
+                System.out.println("Odd");
+            }
+        }
+    }
+}
