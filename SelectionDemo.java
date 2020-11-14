e2fd5ce6cee9f24db6927f4e9cfc0e7a476b5156
bharat <bharatpipare987@gmail.com>
2020-11-14 17:22:41 +0530

    Ternary operator

--- SelectionDemo.java
+++ SelectionDemo.java
@@ -1,14 +1,16 @@
 public class SelectionDemo {
     public static void main(String[] args) {
-        int n = 7;
-        if (n == 0) {
-            System.out.println("Nothing");
-        } else {
-            if (n % 2 == 0) {
-                System.out.println("Even");
-            } else {
-                System.out.println("Odd");
-            }
-        }
+
+        // ternary Operator
+        // ?: --> condition?exp1:exp2;
+        int i = 6;
+        int j = 0;
+
+        /*
+         * if (i>6) { j=1; } else { j=2; }
+         */
+        j = i > 6 ? 1 : 2;
+        System.out.println(j);
+
     }
 }
