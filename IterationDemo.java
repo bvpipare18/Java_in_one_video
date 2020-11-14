57a31814f50ab21b5054e037a6782a9dbf788cf2
bharat <bharatpipare987@gmail.com>
2020-11-14 21:46:25 +0530

    Iterating - while, dowhile, for, for_each

---
+++ IterationDemo.java
@@ -0,0 +1,22 @@
+public class IterationDemo {
+    public static void main(String[] args) {
+        // While Loop
+
+        int counter = 1; // Initialization
+        // while (counter <= 5) { // Condition
+        // System.out.println("Hello Guys");
+        // counter += 1; // Increment
+        // }
+
+        // do while
+        // do {
+        // System.out.println("Hello");
+        // counter += 1;
+        // } while (counter <= 5);
+
+        // for loop
+        for (counter = 1; counter <= 5; counter++) {
+            System.out.println("Hello");
+        }
+    }
+}
