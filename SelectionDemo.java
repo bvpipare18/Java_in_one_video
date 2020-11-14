eaf0dd9931e1301001c89fedcedd57d005572126
bharat <bharatpipare987@gmail.com>
2020-11-14 17:54:48 +0530

    Switch Statement

--- SelectionDemo.java
+++ SelectionDemo.java
@@ -1,16 +1,29 @@
 public class SelectionDemo {
     public static void main(String[] args) {
+        // VALID
+        int n = 4;
+        // char d = 'A';
+        // String s = "Tech";
 
-        // ternary Operator
-        // ?: --> condition?exp1:exp2;
-        int i = 6;
-        int j = 0;
+        switch (n) {
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            case 4:
+                System.out.println("Four");
+                break;
+            case 5:
+                System.out.println("Five");
+                break;
+            default:
+                System.out.println("No match");
 
-        /*
-         * if (i>6) { j=1; } else { j=2; }
-         */
-        j = i > 6 ? 1 : 2;
-        System.out.println(j);
-
+        }
     }
 }
