9be05ff6123705d1735fa625d04a10f3760a5a78
bharat <bharatpipare987@gmail.com>
2020-11-14 22:15:20 +0530

    Loops Assignment

--- IterationDemo.java
+++ IterationDemo.java
@@ -1,22 +1,14 @@
+/**
+ * print the following * * * * * * * * * * * * * * * *
+ */
 public class IterationDemo {
     public static void main(String[] args) {
-        // While Loop
-
-        int counter = 1; // Initialization
-        // while (counter <= 5) { // Condition
-        // System.out.println("Hello Guys");
-        // counter += 1; // Increment
-        // }
-
-        // do while
-        // do {
-        // System.out.println("Hello");
-        // counter += 1;
-        // } while (counter <= 5);
-
-        // for loop
-        for (counter = 1; counter <= 5; counter++) {
-            System.out.println("Hello");
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 4; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
         }
+
     }
 }
