83160523c87cb03bdfc40b62663d32150e196e8d
bharat <bharatpipare987@gmail.com>
2020-11-15 15:06:21 +0530

    break & continue

---
+++ JumpDemo.java
@@ -0,0 +1,20 @@
+public class JumpDemo {
+    // break, continue
+    public static void main(String[] args) {
+        // break
+        // for (int i = 1; i <= 10; i++) {
+        // if (i == 6) {
+        // break;
+        // }
+        // System.out.println("Value is " + i);// Concatenation
+        // }
+
+        // continue
+        // for (int i = 1; i <= 10; i++) {
+        // if (i == 6) {
+        // continue;
+        // }
+        // System.out.println("Value is " + i);// Concatenation
+        // }
+    }
+}
