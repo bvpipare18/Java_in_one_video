23281b8968d9ab3f59b270abd45ebec80bd450f1
bharat <bharatpipare987@gmail.com>
2020-12-19 21:23:51 +0530

    Inheritance - b) Multilevel inheritance

--- InheritanceDemo.java
+++ InheritanceDemo.java
@@ -10,13 +10,21 @@ class CalcAdv extends Calculator {
     }
 }
 
+class CalcVeryAdv extends CalcAdv {
+    public int multi(int i, int j) {
+        return i * j;
+    }
+}
+
 public class InheritanceDemo {
     public static void main(String[] args) {
-        CalcAdv calc = new CalcAdv();
+        CalcVeryAdv calc = new CalcVeryAdv();
         int result1 = calc.add(4, 5);
         int result2 = calc.sub(6, 3);
+        int result3 = calc.multi(6, 3);
 
         System.out.println(result1);
         System.out.println(result2);
+        System.out.println(result3);
     }
 }
