786c3fd29e1aca1d4ca1cf4f39c0e7dfa0aee0e7
bharat <bharatpipare987@gmail.com>
2020-12-19 20:06:24 +0530

    Enhanced for loop

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -2,33 +2,15 @@
 public class ArrayDemo {
     public static void main(String[] args) {
 
-        int d[][] = { { 1, 2, 3, 4 }, { 2, 4, 6, 8 }, { 5, 6, 7, 8 } };// 2D array same no. of elements in row
-
-        System.out.println(d[0][2]);// 0 zero column 2
-
-        for (int i = 0; i < d.length; i++) {// row
-            for (int j = 0; j < d[i].length; j++) {// column
-
-                // These will print on single line
-                // System.out.println(" "+d[i][j]);
-
-                System.out.print(" " + d[i][j]);
-            }
-            System.out.println();
+        int a[] = { 1, 2, 3, 4, 5, 6 };
+        for (int i : a) {
+            System.out.println(i);
         }
 
-        System.out.println();
-
-        int a[][] = { { 1, 2, 3, 4, 5 }, { 2, 4, 6, 8 }, { 5, 6, 7, 8, 9, 10 } }; // Jagged array different no. of array
-                                                                                  // in rows
-
-        for (int i = 0; i < a.length; i++) {// row
-            for (int j = 0; j < a[i].length; j++) {// column
-
-                // These will print on single line
-                // System.out.println(" "+d[i][j]);
-
-                System.out.print(" " + a[i][j]);
+        int d[][] = { { 1, 2, 3, 4, 5 }, { 2, 4, 6, 8 }, { 5, 6, 7, 8, 9, 10 } };
+        for (int k[] : d) {
+            for (int l : k) {
+                System.out.print(" " + l);
             }
             System.out.println();
         }
