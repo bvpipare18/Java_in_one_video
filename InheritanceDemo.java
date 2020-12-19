7623f5d06e3005743a9de78686962368a0ae67c1
bharat <bharatpipare987@gmail.com>
2020-12-19 21:13:05 +0530

    Inheritance - a) Single level inheritance

---
+++ InheritanceDemo.java
@@ -0,0 +1,22 @@
+class Calculator {
+    public int add(int i, int j) {
+        return i + j;
+    }
+}
+
+class CalcAdv extends Calculator {
+    public int sub(int i, int j) {
+        return i - j;
+    }
+}
+
+public class InheritanceDemo {
+    public static void main(String[] args) {
+        CalcAdv calc = new CalcAdv();
+        int result1 = calc.add(4, 5);
+        int result2 = calc.sub(6, 3);
+
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+}
